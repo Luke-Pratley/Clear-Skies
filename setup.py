994d7e4ec3f06d84346a62c544990ccf383234df
Luke Pratley <Luke.Pratley@gmail.com>
2020-04-13 21:43:59 -0400

    added first test

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="clearskies",
+    version="0.0.1",
+    author="Luke Pratley",
+    author_email="luke.pratley@gmail.com",
+    description="Convex Denoiser",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Luke-Pratley/optimusprimal",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+          'numpy',
+          'PyWavelets',
+          'optimiusprimal'
+      ]
+)
+
---
+++ tests/solver_test.py
@@ -0,0 +1,40 @@
+import sys
+sys.path.insert(0, '..')
+import numpy as np
+from clearskies import solver
+from astropy.io import fits
+
+
+def open_fits(image_file):
+    hdu_list = fits.open(image_file)
+    return hdu_list[0].data
+
+
+def open_header(image_file):
+    hdu_list = fits.open(image_file)
+    return hdu_list[0].header
+
+
+
+def test_constrained():
+    input_file = "../data/lmc.fits"
+    x_true = open_fits(input_file)
+    
+    options = {'tol': 1e-5, 'iter': 5000, 'update_iter': 50, 'record_iters': False}
+    ISNR = 10.
+    sigma = 10**(-ISNR/20.) * np.sqrt(np.sum(np.abs(x_true)**2)/(x_true.shape[0] * x_true.shape[1]))
+    width, height = x_true.shape 
+
+    W = np.ones(x_true.shape)
+    
+    y = W * x_true + np.random.normal(0, sigma, x_true.shape)
+    
+    
+    wav = ["db1", "db2", "db3", "db4", "db5", "db6", "db7", "db8"]
+    levels = 4
+    
+    z, diag = solver.solver(solver.algorithm.constrained, y, sigma, W, wav, levels, 1e-3, options)
+    SNR = np.log10(np.sqrt(np.sum(np.abs(x_true)**2))/np.sqrt(np.sum(np.abs(x_true - z)**2))) * 20.
+    assert(SNR > INSR)
+    size = z.shape[0] * z.shape[1]
+    assert(np.linalg.norm(W * z - y) < np.sqrt(size + 2 * np.sqrt(size)) * sigma)
