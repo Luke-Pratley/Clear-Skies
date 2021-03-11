926530523a57876b2c83551809c1ec545a77d7d8
Luke Pratley <Luke.Pratley@gmail.com>
2021-03-11 14:15:26 -0500

    added TV norm and bbs solvers

--- clearskies/core.py
+++ clearskies/core.py
@@ -103,3 +103,194 @@ def l1_poissonian_unconstrained_solver(data, warm_start, weights, background, ps
             raise Exception("Positivity required.")
     f = None
     return primal_dual.FBPD(warm_start, options, None, f, h, p)
+
+def tv_constrained_solver(data, warm_start, sigma, weights, beta=1e-3, options={'tol': 1e-5, 'iter': 5000, 'update_iter': 50, 'record_iters': False, 'positivity': False, 'real': False}):
+    """
+    Solve constrained tv regularization problem
+    """
+    phi = linear_operators.diag_matrix_operator(weights)
+    size = len(np.ravel(data))
+    epsilon = np.sqrt(size + 2 * np.sqrt(2 * size)) * sigma
+    p = prox_operators.l2_ball(epsilon, data, phi)
+    p.beta = np.max(np.abs(weights))**2
+    f = None
+    if options['real'] == True:
+        f = prox_operators.real_prox()
+    if options["positivity"] == True:
+        f = prox_operators.positive_prox()
+    def forward(x):
+        if x.ndim == 2:
+            out = np.zeros((2, x.shape[0] - 1, x.shape[1] - 1))
+            out[0, :, :] = (x[:-1, :-1] - x[1:, :-1])/2
+            out[1, :, :] = (x[:-1, :-1] - x[:-1, 1:])/2
+            return out
+        else:
+            raise Exception("Sorry, only two dimensions for now.")
+    def backward(x):
+        if x.ndim == 3:
+            out = np.zeros((x.shape[1] + 1, x.shape[2] + 1))
+            out[:-1, :-1] +=  x[0, :, :]/2
+            out[:-1, :-1] +=  x[1, :, :]/2
+            out[1:, :-1] +=  -x[0, :, :]/2
+            out[:-1, 1:] +=  -x[1, :, :]/2
+            return out
+        else:
+            raise Exception("Sorry, only two dimensions for now.")
+    psi = linear_operators.function_wrapper(forward, backward)
+    h = prox_operators.l21_norm(np.sqrt(np.max(np.sum(np.abs(psi.dir_op(data))**2), axis=0)) * beta, 0, psi)
+    return primal_dual.FBPD(warm_start, options, None, f, h, p)
+
+def tv_unconstrained_solver(data, warm_start, sigma, weights, beta=1e-3, options={'tol': 1e-5, 'iter': 5000, 'update_iter': 50, 'record_iters': False, 'positivity': False, 'real': False}):
+    """
+    Solve unconstrained l1 regularization problem
+    """
+
+    phi = linear_operators.diag_matrix_operator(weights)
+    g = grad_operators.l2_norm(sigma, data, phi)
+    g.beta = np.max(np.abs(weights))**2 / sigma**2
+    if beta <= 0:
+        h = None
+    else:
+        def forward(x):
+            if x.ndim == 2:
+                out = np.zeros((2, x.shape[0] - 1, x.shape[1] - 1))
+                out[0, :, :] = (x[:-1, :-1] - x[1:, :-1])/2
+                out[1, :, :] = (x[:-1, :-1] - x[:-1, 1:])/2
+                return out
+            else:
+                raise Exception("Sorry, only two dimensions for now.")
+        def backward(x):
+            if x.ndim == 3:
+                out = np.zeros((x.shape[1] + 1, x.shape[2] + 1))
+                out[:-1, :-1] +=  x[0, :, :]/2
+                out[:-1, :-1] +=  x[1, :, :]/2
+                out[1:, :-1] +=  -x[0, :, :]/2
+                out[:-1, 1:] +=  -x[1, :, :]/2
+                return out
+            else:
+                raise Exception("Sorry, only two dimensions for now.")
+        psi = linear_operators.function_wrapper(forward, backward)
+        h = prox_operators.l21_norm(beta, 0, psi)
+    f = None
+    if options['real'] == True:
+        if options["positivity"] == True:
+            f = prox_operators.positive_prox()
+        else:
+            f = prox_operators.real_prox()
+    return primal_dual.FBPD(warm_start, options, g, f, h)
+
+def l1_constrained_separation_solver(data, warm_start, sigma, weights, psi1, psi2, gamma=1, beta=1e-3, options={'tol': 1e-5, 'iter': 5000, 'update_iter': 50, 'record_iters': False, 'positivity': False, 'real': False}):
+    """
+    Solve constrained l1 regularization problem for signal separation
+    """
+    def m_forward(x):
+        return (x[0, :, :] + x[1, :, :]) * weights
+    def m_backward(x):
+        out = np.zeros((2, x.shape[0], x.shape[1]), dtype=complex)
+        out[0] = x * weights
+        out[1] = x * weights
+        return out
+
+    phi = linear_operators.function_wrapper(m_forward, m_backward)
+    size = len(np.ravel(data))
+    epsilon = np.sqrt(size + 2 * np.sqrt(2 * size)) * sigma
+    p = prox_operators.l2_ball(epsilon, data, phi)
+    p.beta = np.max(np.abs(weights))**2 * 4
+    f = None
+    if options['real'] == True:
+        f = prox_operators.real_prox()
+    if options["positivity"] == True:
+        f = prox_operators.positive_prox()
+    def w1_forward(x):
+        return psi1.dir_op(x[0, :, :])
+    def w2_forward(x):
+        return psi2.dir_op(x[1, :, :])
+    def w1_backward(x):
+        out = psi1.adj_op(x)
+        buff = np.zeros((2, out.shape[0], out.shape[1]), dtype=complex)
+        buff[0] = out
+        return buff
+    def w2_backward(x):
+        out = psi2.adj_op(x)
+        buff = np.zeros((2, out.shape[0], out.shape[1]), dtype=complex)
+        buff[1] = out
+        return buff
+
+    psi1_wrapper = linear_operators.function_wrapper(w1_forward, w1_backward)
+    psi2_wrapper = linear_operators.function_wrapper(w2_forward, w2_backward)
+
+    h = prox_operators.l1_norm(np.max(np.abs(psi1.dir_op(data))) * beta, psi1_wrapper)
+    h.beta = 1
+    r = prox_operators.l1_norm(np.max(np.abs(psi2.dir_op(data))) * beta * gamma, psi2_wrapper)
+    r.beta = 1
+    out = np.zeros((2, warm_start.shape[0], warm_start.shape[1]), dtype=complex)
+    out[0] = warm_start
+    out[1] = warm_start
+    return primal_dual.FBPD(out, options, None, f, h, p, r)
+
+def tv_constrained_separation_solver(data, warm_start, sigma, weights, psi1, gamma=1, beta=1e-3, options={'tol': 1e-5, 'iter': 5000, 'update_iter': 50, 'record_iters': False, 'positivity': False, 'real': False}):
+    """
+    Solve constrained l1 regularization problem for signal separation
+    """
+    def m_forward(x):
+        return (x[0, :, :] + x[1, :, :]) * weights
+    def m_backward(x):
+        out = np.zeros((2, x.shape[0], x.shape[1]), dtype=complex)
+        out[0] = x * weights
+        out[1] = x * weights
+        return out
+
+    phi = linear_operators.function_wrapper(m_forward, m_backward)
+    size = len(np.ravel(data))
+    epsilon = np.sqrt(size + 2 * np.sqrt(2 * size)) * sigma
+    p = prox_operators.l2_ball(epsilon, data, phi)
+    p.beta = np.max(np.abs(weights))**2 * 4
+    f = None
+    if options['real'] == True:
+        f = prox_operators.real_prox()
+    if options["positivity"] == True:
+        f = prox_operators.positive_prox()
+    def forward(x):
+        if x.ndim == 2:
+            out = np.zeros((2, x.shape[0] - 1, x.shape[1] - 1))
+            out[0, :, :] = (x[:-1, :-1] - x[1:, :-1])/2
+            out[1, :, :] = (x[:-1, :-1] - x[:-1, 1:])/2
+            return out
+        else:
+            raise Exception("Sorry, only two dimensions for now.")
+    def backward(x):
+        if x.ndim == 3:
+            out = np.zeros((x.shape[1] + 1, x.shape[2] + 1))
+            out[:-1, :-1] +=  x[0, :, :]/2
+            out[:-1, :-1] +=  x[1, :, :]/2
+            out[1:, :-1] +=  -x[0, :, :]/2
+            out[:-1, 1:] +=  -x[1, :, :]/2
+            return out
+        else:
+            raise Exception("Sorry, only two dimensions for now.")
+    def w1_forward(x):
+        return psi1.dir_op(x[0, :, :])
+    def w2_forward(x):
+        return forward(x[1, :, :])
+    def w1_backward(x):
+        out = psi1.adj_op(x)
+        buff = np.zeros((2, out.shape[0], out.shape[1]), dtype=complex)
+        buff[0] = out
+        return buff
+    def w2_backward(x):
+        out = backward(x)
+        buff = np.zeros((2, out.shape[0], out.shape[1]), dtype=complex)
+        buff[1] = out
+        return buff
+
+    psi1_wrapper = linear_operators.function_wrapper(w1_forward, w1_backward)
+    psi2_wrapper = linear_operators.function_wrapper(w2_forward, w2_backward)
+
+    h = prox_operators.l1_norm(np.max(np.abs(psi1.dir_op(data))) * beta, psi1_wrapper)
+    h.beta = 1
+    r = prox_operators.l21_norm(np.sqrt(np.max(np.sum(np.abs(forward(data))**2,axis=0))) * beta * gamma, 0, psi2_wrapper)
+    r.beta = 1
+    out = np.zeros((2, warm_start.shape[0], warm_start.shape[1]), dtype=complex)
+    out[0] = warm_start
+    out[1] = warm_start
+    return primal_dual.FBPD(out, options, None, f, h, p, r)
--- clearskies/solver.py
+++ clearskies/solver.py
@@ -11,6 +11,9 @@ class algorithm(Enum):
     l2_unconstrained = 2
     l1_poisson_constrained = 3
     l1_poisson_unconstrained = 4
+    tv_constrained = 5
+    tv_unconstrained = 6
+    l1_constrained_separation = 7
 
 
 logger = logging.getLogger('Clear Skies')
@@ -59,4 +62,33 @@ def solver(algo, image, sigma, weights, wav=["dirac"], levels=6, beta=1e-3, opti
         else:
             background = image * 0
         return clearskies.core.l1_poissonian_unconstrained_solver(data, starting_data, weights, background, psi, beta, options)
+    if algo == algorithm.tv_constrained:
+        logger.info("Denosing using constrained tv regularization")
+        return clearskies.core.tv_constrained_solver(data, starting_data, sigma, weights, beta, options)
+    if algo == algorithm.tv_unconstrained:
+        logger.info("Denosing using unconstrained tv regularization")
+        return clearskies.core.tv_unconstrained_solver(data, starting_data, sigma, weights, beta, options)
     raise ValueError("Algorithm not reconginized.")
+
+def separation_solver(algo, image, sigma, weights, wav1=["dirac"], wav2=["fourier"], levels=6, gamma=1, beta=1e-3, options={'tol': 1e-5, 'iter': 5000, 'update_iter': 50, 'record_iters': False, "positivity": False},  warm_start=None, background=None, axes=None):
+    """
+    Chooses algorithm and solver
+    """
+    logger.info("Image shape %s", image.shape)
+    logger.info("Using wavelets %s with %s levels", wav1, levels)
+    logger.info("Using wavelets %s with %s levels", wav2, levels)
+    logger.info(
+        "Using an estimated noise level of %s (weighted image units, i.e. Jy/Beam)", sigma)
+    psi1 = linear_operators.dictionary(wav1, levels, image.shape, axes)
+    psi2 = linear_operators.dictionary(wav2, levels, image.shape, axes)
+    data = image * weights
+    starting_data = data * weights
+    if(warm_start is not None):
+        logger.info("Using warm start.")
+        starting_data = warm_start
+    if algo == algorithm.l1_constrained:
+        logger.info("Signal separation using constrained l1 regularization")
+        return clearskies.core.l1_constrained_separation_solver(data, starting_data, sigma, weights, psi1, psi2, gamma, beta, options)
+    if algo == algorithm.tv_constrained:
+        logger.info("Signal separation using constrained tv regularization")
+        return clearskies.core.tv_constrained_separation_solver(data, starting_data, sigma, weights, psi1, gamma, beta, options)
